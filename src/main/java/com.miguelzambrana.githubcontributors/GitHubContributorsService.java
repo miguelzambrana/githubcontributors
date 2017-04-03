d121d9d3af9f2f728277940d92b30d073170feca
Miki <mzambrana@nicepeopleatwork.com>
2017-04-03 19:57:30 +0200

    Initial commit; Hello world!!
    Initial commit; Hello world!!

---
+++ src/main/java/com.miguelzambrana.githubcontributors/GitHubContributorsService.java
@@ -0,0 +1,14 @@
+package com.miguelzambrana.githubcontributors;
+
+/**
+ * Created by miki on 3/04/17.
+ */
+public class GitHubContributorsService {
+
+    public static void main ( String[] args ) {
+
+        System.out.println("Hi I'm GihHubContributorsService!!");
+
+    }
+
+}
